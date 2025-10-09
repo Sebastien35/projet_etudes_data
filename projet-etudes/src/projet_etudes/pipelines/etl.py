fc5af45876dcdef0f181b77a21aa11da6634931d
SebastienBorgne <sebastien.borgne@groupeavril.com>
2025-10-09 14:58:49 +0200

    feat: atproto bsky conn ok

--- projet-etudes/src/projet_etudes/pipelines/etl.py
+++ projet-etudes/src/projet_etudes/pipelines/etl.py
@@ -1,4 +1,20 @@
+import os
+
 from atproto import Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+BSKY_PWD = os.getenv('BSKY_APP_PASSWORD')
+BSKY_USERNAME = os.getenv('BSKY_USERNAME')
+
+if( not BSKY_PWD or not BSKY_USERNAME):
+    raise ValueError("BSKY_PWD and BSKY_USERNAME must be set in environment variables")
 
 client = Client()
-client.login()
+client.login(BSKY_USERNAME, BSKY_PWD)
+
+
+
+
