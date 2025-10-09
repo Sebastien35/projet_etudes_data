6269c5f034d3c7eee7949555050bcd330debbbc4
SebastienBorgne <sebastien.borgne@groupeavril.com>
2025-10-09 16:51:17 +0200

    wip

--- projet-etudes/src/projet_etudes/pipelines/etl.py
+++ projet-etudes/src/projet_etudes/pipelines/etl.py
@@ -2,10 +2,17 @@ import os
 
 from atproto import Client
 from dotenv import load_dotenv
+from pymongo import MongoClient
+
 
 load_dotenv()
 
-
+def get_mongo_conn():
+    MONGO_CONNECTION_STRING = os.getenv("MONGO_CONNECTION_STRING")
+    mongo_client = MongoClient(MONGO_CONNECTION_STRING)
+    db = mongo_client["bluesky_db"]
+    posts_collection = db["posts"]  # This will use or create the 'posts' collection
+    return posts_collection
 
 
 def get_client():
@@ -21,16 +28,19 @@ def get_client():
     return client
 
 
-def get_data(client: Client, BSKY_USERNAME: str):
-    """Fetch data from Bluesky API."""
-    # Example: Fetch the user's profile information
-    profile = client.get_profile(BSKY_USERNAME)
-    return profile
+def get_post_texts(client: Client, username: str, limit: int = 10):
+    """Fetch post texts from a Bluesky user."""
+    feed = client.app.bsky.feed.get_author_feed({
+        'actor': username,
+        'limit': limit
+    })
+    return [item['post']['record']['text'] for item in feed['feed']]
 
 client = get_client()
-data = get_data(client, os.getenv('BSKY_USERNAME'))
-print(data)
+data = get_post_texts(client, 'gtconway.bsky.social', limit=5)
 
-
-
+for post in data:
+    save_to_db = {'text': post}
+    conn = get_mongo_conn()
+    conn.insert_one(save_to_db)
 
